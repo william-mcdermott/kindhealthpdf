c23bea766c370e8825f3570a2095e3083dee8c09
Christian Davis <Christian@kindhealth.co>
2016-09-16 14:43:05 -0500

    Add test, corrected route code

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 var parsedJSON = require('./data/fields.json');
 var pdfFiller = require('pdffiller');
 var fs = require('fs');
+var tmp_dir = 'tmp/'
 
 module.exports = (app) =>{
 
@@ -10,19 +11,34 @@ module.exports = (app) =>{
       return
     }
     let sampleFile = req.files.file
-    console.log(sampleFile);
+
     if (!sampleFile) {
-      res.status(500).send('err')
-      return
-    }else {
-      var nameRegex = null;
-      // Here instead of write a file, do your PDF map work, return the fields
-      var FDF_data = pdfFiller.generateFDFTemplate(sampleFile, nameRegex, function(err, fdfData) {
-        if (err) throw err;
-        res.status(200).json(FDF_data);
-      // Instead of send this, send your JSON field data
-      // res.send('File uploaded!')
-    });
+      return res.status(500).send('err')
+      
+    } else {
+
+      // try to run code that might fail disastrously 
+      try {
+        let writeFilePath = tmp_dir+sampleFile.name
+        // Have to save the file for the pdftk bin
+        fs.writeFile(writeFilePath, sampleFile.data, function (err) {
+          if (err) return console.log(err)
+          console.log('File written')
+
+          var nameRegex = null;
+          // Here instead of write a file, do your PDF map work, return the fields
+          pdfFiller.generateFDFTemplate(writeFilePath, nameRegex, function (err, fdfData) {
+            if (err) throw err;
+            console.log("FDF fields parsed")
+            return res.status(200).json(fdfData);
+          })
+        })
+        
+      }catch(err){ // if something goes wrong in the try, return 500
+        console.log(err)
+        return res.status(500).json("Server failed to parse PDF");
+      }
+     
     }
   });
 
